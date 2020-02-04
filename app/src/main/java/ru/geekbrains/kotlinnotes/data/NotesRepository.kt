5cb77ff89ee5e5bcb1ca3c4d679dd8b9ac10cf1b
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-02-04 22:37:16 +0300

    В репозитори тепрь есть LiveData, методы по обновлению списка заметок

--- app/src/main/java/ru/geekbrains/kotlinnotes/data/NotesRepository.kt
+++ app/src/main/java/ru/geekbrains/kotlinnotes/data/NotesRepository.kt
@@ -1,14 +1,71 @@
 package ru.geekbrains.kotlinnotes.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import ru.geekbrains.kotlinnotes.data.entity.Note
+import java.util.*
 
 object NotesRepository {
-    val notes: List<Note> = listOf(
-        Note("Note 1 title", "Note 1 text", 0xfff06292.toInt()),
-        Note("Note 2 title", "Note 2 text", 0xff9575cd.toInt()),
-        Note("Note 3 title", "Note 3 text", 0xff64b5f6.toInt()),
-        Note("Note 4 title", "Note 4 text", 0xff4db6ac.toInt()),
-        Note("Note 5 title", "Note 5 text", 0xffb2ff59.toInt()),
-        Note("Note 6 title", "Note 6 text", 0xffffeb3b.toInt())
+    private val notesLiveData = MutableLiveData<List<Note>>()
+
+    private val notes: MutableList<Note> = mutableListOf(
+        Note(
+            UUID.randomUUID().toString(),
+            "Note 1 title", "Note 1 text",
+            Note.Color.WHITE
+        ),
+        Note(
+            UUID.randomUUID().toString(),
+            "Note 2 title", "Note 2 text",
+            Note.Color.YELLOW
+        ),
+        Note(
+            UUID.randomUUID().toString(),
+            "Note 3 title", "Note 3 text",
+            Note.Color.GREEN
+        ),
+        Note(
+            UUID.randomUUID().toString(),
+            "Note 4 title", "Note 4 text",
+            Note.Color.BLUE
+        ),
+        Note(
+            UUID.randomUUID().toString(),
+            "Note 4 title", "Note 4 text",
+            Note.Color.RED
+        ),
+        Note(
+            UUID.randomUUID().toString(),
+            "Note 5 title", "Note 5 text",
+            Note.Color.VIOLET
+        ),
+        Note(
+            UUID.randomUUID().toString(),
+            "Note 6 title", "Note 6 text",
+            Note.Color.PINK
+        )
     )
+
+    init {
+        notesLiveData.value = notes
+    }
+
+    fun saveNote(note: Note){
+        addOrReplace(note)
+        notesLiveData.value = notes
+    }
+
+    private fun addOrReplace(note: Note){
+        for(i in notes.indices){
+            if(notes[i] == note){
+                notes[i] = note
+                return
+            }
+        }
+        notes.add(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>> {
+        return notesLiveData
+    }
 }
