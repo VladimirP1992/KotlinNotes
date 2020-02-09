f0de7c243aae6fd24433bbe8dcb3da87751579c3
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-02-09 22:39:26 +0300

    ViewModels's активити теперь работают с NoteResult

--- app/src/main/java/ru/geekbrains/kotlinnotes/ui/note/NoteEditActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlinnotes/ui/note/NoteEditActivity.kt
@@ -1,10 +1,8 @@
-package ru.geekbrains.kotlinnotes.ui
+package ru.geekbrains.kotlinnotes.ui.note
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.MenuItem
@@ -13,54 +11,58 @@ import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_note_edit.*
 import ru.geekbrains.kotlinnotes.R
 import ru.geekbrains.kotlinnotes.data.entity.Note
+import ru.geekbrains.kotlinnotes.ui.base.BaseActivity
 import java.text.SimpleDateFormat
 import java.util.*
 
-class NoteEditActivity : AppCompatActivity() {
-
+class NoteEditActivity : BaseActivity<Note?, NoteEditViewState>() {
     companion object {
         private val EXTRA_NOTE = NoteEditActivity::class.java.name + "extra.NOTE"
         private const val DATE_TIME_FORMAT = "dd.MM.yy HH:mm"
-        private const val SAVE_DELAY = 2000L
 
-        fun start(context: Context, note: Note? = null) {
+        fun start(context: Context, noteId: String? = null) {
             val intent = Intent(context, NoteEditActivity::class.java)
-            intent.putExtra(EXTRA_NOTE, note)
+            intent.putExtra(EXTRA_NOTE, noteId)
             context.startActivity(intent)
         }
     }
 
+    override val layoutRes = R.layout.activity_note_edit
+    override val viewModel: NoteEditViewModel by lazy { ViewModelProvider(this).get(NoteEditViewModel::class.java) }
     private var note: Note? = null
-    private lateinit var viewModel: NoteEditViewModel
 
-    private val textChangeListener = object : TextWatcher {
+    val textChahgeListener = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         override fun afterTextChanged(s: Editable?) {
             saveNote()
         }
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_edit)
-
-        note = intent.getParcelableExtra(EXTRA_NOTE)
         setSupportActionBar(toolbar)
-        supportActionBar?.setDefaultDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        viewModel = ViewModelProvider(this).get(NoteEditViewModel::class.java)
+        val noteId = intent.getStringExtra(EXTRA_NOTE)
 
-        supportActionBar?.title = note?.let {
-            SimpleDateFormat(DATE_TIME_FORMAT, Locale.getDefault()).format(it.lastChanged)
+        noteId?.let {
+            viewModel.loadNote(it)
+        } ?: let {
+            supportActionBar?.title = getString(R.string.defaultNoteTitle)
+        }
+    }
+
+    override fun renderData(data: Note?) {
+        this.note = data
+        supportActionBar?.title = this.note?.let {
+            SimpleDateFormat(DATE_TIME_FORMAT, Locale.getDefault()).format(note!!.lastChanged)
         } ?: getString(R.string.defaultNoteTitle)
 
         initView()
     }
 
-    private fun initView() {
+    fun initView() {
         note?.let { note ->
             ane_title.setText(note.title)
             ane_body.setText(note.body)
@@ -77,30 +79,29 @@ class NoteEditActivity : AppCompatActivity() {
             toolbar.setBackgroundColor(ContextCompat.getColor(this, color))
         }
 
-        ane_title.addTextChangedListener(textChangeListener)
-        ane_body.addTextChangedListener(textChangeListener)
+        ane_title.addTextChangedListener(textChahgeListener)
+        ane_body.addTextChangedListener(textChahgeListener)
     }
 
     fun saveNote() {
         if (ane_title.text == null || ane_title.text!!.length < 3) return
 
-        Handler().postDelayed({
-            note = note?.copy(
-                title = ane_title.text.toString(),
-                body = ane_body.text.toString(),
-                lastChanged = Date()
-            ) ?: createNewNote()
+        note = note?.copy(
+            title = ane_title.text.toString(),
+            body = ane_body.text.toString(),
+            lastChanged = Date()
+        ) ?: Note(
+            UUID.randomUUID().toString(),
+            ane_title.text.toString(),
+            ane_body.text.toString()
+        )
 
-            note?.let { viewModel.save(it) }
-
-        }, SAVE_DELAY)
+        note?.let { viewModel.save(it) }
     }
-
-    private fun createNewNote(): Note = Note(UUID.randomUUID().toString(), ane_title.text.toString(), ane_body.text.toString())
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         android.R.id.home -> {
-            onBackPressed()
+            onBackPressed();
             true
         }
         else -> super.onOptionsItemSelected(item)
