a94f5d2570822bb5a61e170254bab94b6baa875a
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-02-09 22:20:20 +0300

    NoteRepository теперь работает через FireStoreProvider

--- app/src/main/java/ru/geekbrains/kotlinnotes/data/NotesRepository.kt
+++ app/src/main/java/ru/geekbrains/kotlinnotes/data/NotesRepository.kt
@@ -1,71 +1,15 @@
 package ru.geekbrains.kotlinnotes.data
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import ru.geekbrains.kotlinnotes.data.entity.Note
-import java.util.*
+import ru.geekbrains.kotlinnotes.data.provider.FireStoreProvider
+import ru.geekbrains.kotlinnotes.data.provider.RemoteDataProvider
+
 
 object NotesRepository {
-    private val notesLiveData = MutableLiveData<List<Note>>()
 
-    private val notes: MutableList<Note> = mutableListOf(
-        Note(
-            UUID.randomUUID().toString(),
-            "Note 1 title", "Note 1 text",
-            Note.Color.WHITE
-        ),
-        Note(
-            UUID.randomUUID().toString(),
-            "Note 2 title", "Note 2 text",
-            Note.Color.YELLOW
-        ),
-        Note(
-            UUID.randomUUID().toString(),
-            "Note 3 title", "Note 3 text",
-            Note.Color.GREEN
-        ),
-        Note(
-            UUID.randomUUID().toString(),
-            "Note 4 title", "Note 4 text",
-            Note.Color.BLUE
-        ),
-        Note(
-            UUID.randomUUID().toString(),
-            "Note 4 title", "Note 4 text",
-            Note.Color.RED
-        ),
-        Note(
-            UUID.randomUUID().toString(),
-            "Note 5 title", "Note 5 text",
-            Note.Color.VIOLET
-        ),
-        Note(
-            UUID.randomUUID().toString(),
-            "Note 6 title", "Note 6 text",
-            Note.Color.PINK
-        )
-    )
+    private val remoteProvider: RemoteDataProvider = FireStoreProvider()
 
-    init {
-        notesLiveData.value = notes
-    }
-
-    fun saveNote(note: Note){
-        addOrReplace(note)
-        notesLiveData.value = notes
-    }
-
-    private fun addOrReplace(note: Note){
-        for(i in notes.indices){
-            if(notes[i] == note){
-                notes[i] = note
-                return
-            }
-        }
-        notes.add(note)
-    }
-
-    fun getNotes(): LiveData<List<Note>> {
-        return notesLiveData
-    }
+    fun getNotes() = remoteProvider.subsrcibeToAllNotes()
+    fun saveNote(note: Note) = remoteProvider.saveNote(note)
+    fun getNoteById(id: String) = remoteProvider.getNoteById(id)
 }
